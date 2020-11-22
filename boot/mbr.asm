a426a4d2ed71f94354e4d9d6d217c73d9a33c099
ValjeanTheWolfie <franciscowp@qq.com>
2020-11-22 23:39:04 +0800

    修正FAT表项载入时的处理错误

--- boot/mbr.asm
+++ boot/mbr.asm
@@ -25,22 +25,23 @@ org 0x7c00
     mov ah, 06h
     mov al, 00h
     int 10h
-
     ; Reset cursor
     mov ax, 02h
     mov bx, 0
     mov dx, 0
     int 10h
-
     ; Print message
-    mov si, start_message
-    call print
+    mov si, message_start
+    call fat12_print
 
 
     mov ax, loader_filename
     call fat12_search_file_in_rootdir
     cmp ax, 0
     jz loader_not_found
+
+    mov si, message_loader
+    call fat12_print
 
     mov bx, 0x1000
     mov fs, bx
@@ -49,15 +50,16 @@ org 0x7c00
     jmp 0x1000:0
 
 loader_not_found:
-    mov si, loader_not_found_message
-    call print
+    mov si, message_loader_not_found
+    call fat12_print
     jmp $
 
 ; ==================
 ;    Data part
 ; ==================
-    start_message db "Starting TinySYS...", CR, LF, 0
-    loader_not_found_message db "loader.bin not found. System halt.", CR, LF, 0
+    message_start            db "Welcome!", CR, LF, 0
+    message_loader           db "Starting loader",0
+    message_loader_not_found db "Loader.bin is missing!", 0
 
     loader_filename  db "LOADER  BIN"
 
