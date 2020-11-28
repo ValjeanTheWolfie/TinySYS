f727f37853b29c9086261a3faa9a406b3acc680f
ValjeanTheWolfie <franciscowp@qq.com>
2020-11-28 22:20:05 +0800

    编写makefile

--- formatFAT12.sh
+++
@@ -1,8 +0,0 @@
-rm -f build/*.bin
-echo "times 512*50 db 0" > empty.asm
-nasm empty.asm -o empty.bin
-dd if=empty.bin of=build/fd144.img bs=$[512*50] count=1 conv=notrunc 
-rm empty*
-
-nasm boot/mbr.asm -i boot/ -o build/mbr.bin 
-dd if=build/mbr.bin of=build/fd144.img bs=512 count=1 conv=notrunc 
