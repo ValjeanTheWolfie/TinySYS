5be4f928ca7b534bd5c82a839d8254086266d5f4
ValjeanTheWolfie <franciscowp@qq.com>
2020-12-13 21:38:16 +0800

    添加字符串操作函数strlen, strcpy, strcat等

--- kernel/string.h
+++ kernel/string.h
@@ -78,6 +78,153 @@ static inline void* memset(void * dest,unsigned char val,unsigned long size)
     return dest;
 }
 
+static inline unsigned long strlen(const char *s)
+{
+    long d0;
+    register unsigned long res;
+    __asm__ __volatile__
+    (
+        "repne scasb \n\t"
+        "not %0 \n\t"
+        "dec %0 \n\t"
+        :"=c"(res), "=&D"(d0)
+        :"1"(s),"a"(0), "0"((long)-1)
+    );
+
+    return res;
+}
+
+static inline char* strcpy(char *dest, const char *src)
+{
+    long d0, d1, d2;
+    __asm__ __volatile__
+	(
+        "1: lodsb \n\t"
+        "stosb \n\t"
+        "testb %%al,%%al \n\t"
+        "jne 1b"
+        :"=&S"(d0), "=&D"(d1), "=&a"(d2)
+        :"0"(src),"1"(dest) 
+        :"memory"
+	);
+    return dest;
+}
+
+static inline char* strncpy(char * dest, const char *src, unsigned long length)
+{
+    long d0, d1, d2, d3;
+    __asm__ __volatile__
+	(
+        "1: dec %2 \n\t"
+        "js 2f \n\t"
+        "lodsb \n\t"
+        "stosb \n\t"
+        "testb %%al, %%al \n\t"
+        "jne 1b \n\t"
+        "rep \n\t"
+        "stosb\n"
+        "2:"
+        :"=&S"(d0), "=&D"(d1), "=&c"(d2), "=&a"(d3)
+        :"0"(src),"1"(dest),"2"(length) : "memory"
+	);
+
+    return dest;
+}
+
+static inline char* strcat(char *dest, const char *src)
+{
+    long d0, d1, d2, d3;
+    __asm__ __volatile__
+	(
+        "repne scasb \n\t"
+        "dec %1\n"
+        "1: \n\t"
+        "lodsb \n\t"
+        "stosb \n\t"
+        "testb %%al, %%al \n\t"
+        "jne 1b \n\t"
+        :"=&S"(d0), "=&D"(d1), "=&a"(d2), "=&c"(d3)
+        :"0"(src), "1"(dest), "2"(0), "3"(0xffffffffffffffffUL)
+        :"memory"
+	);
+
+    return dest;
+}
+
+static inline char* strncat(char *dest, const char *src, unsigned long length)
+{
+    long d0, d1, d2, d3;
+    __asm__ __volatile__
+	(
+        "repne \n\t"
+        "scasb \n\t"
+        "dec %1 \n\t"
+        "mov %4, %3\n"
+        "1: dec %3 \n\t"
+        "js 2f \n\t"
+        "lodsb \n\t"
+        "stosb \n\t"
+        "testb %%al, %%al \n\t"
+        "jne 1b\n"
+        "2: xor %2, %2 \n\t"
+        "stosb"
+        :"=&S"(d0), "=&D"(d1), "=&a"(d2), "=&c"(d3)
+        :"g"(length), "0"(src), "1"(dest), "2"(0), "3"(0xffffffff)
+        :"memory"
+	);
+	
+    return dest;
+}
+
+static inline int strcmp(const char *str1,const char *str2)
+{
+    long d0, d1;
+    register int res;
+    __asm__ __volatile__
+    (
+        "1: lodsb \n\t"
+        "scasb \n\t"
+        "jne 2f \n\t"
+        "testb %%al, %%al \n\t"
+        "jne 1b \n\t"
+        "xorl %%eax, %%eax \n\t"
+        "jmp 3f\n"
+        "2: sbbl %%eax, %%eax \n\t"
+        "orb $1,%%al\n"
+        "3:"
+        :"=a"(res), "=&S"(d0), "=&D"(d1)
+        :"1"(str1), "2"(str2)
+    );
+	
+    return res;
+}
+
+static inline int strncmp(const char *str1,const char *str2, unsigned long count)
+{
+    register int res;
+    int d0, d1, d2;
+    __asm__ __volatile__
+    (
+        "1: dec %3 \n\t"
+        "js 2f \n\t"
+        "lodsb \n\t"
+        "scasb \n\t"
+        "jne 3f \n\t"
+        "testb %%al, %%al \n\t"
+        "jne 1b\n"
+        "2: xorl %%eax, %%eax \n\t"
+        "jmp 4f\n"
+        "3: sbbl %%eax, %%eax \n\t"
+        "orb $1, %%al\n"
+        "4:"
+        :"=a"(res), "=&S"(d0), "=&D"(d1), "=&c"(d2)
+        :"1"(str1),"2"(str2),"3"(count)
+    );
+
+    return res;
+}
+
+
 
 
 
