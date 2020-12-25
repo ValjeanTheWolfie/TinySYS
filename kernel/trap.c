f18c84ffc1215b45cc8ea623fc3f323d2aa38fdc
ValjeanTheWolfie <franciscowp@qq.com>
2020-12-26 01:47:50 +0800

    完善异常提示信息

--- kernel/trap.c
+++ kernel/trap.c
@@ -3,6 +3,41 @@
 #include "lib.h"
 #include "printk.h"
 
+IdtEntryInfo idtEntry[] = {
+    {/*0 */ idt_ent_0 , "[0] Divide Error",                               ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*1 */ idt_ent_1 , "[1] Debug Exception",                            ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*2 */ idt_ent_2 , "[2] NMI",                                        ACCESS_BYTE_INTERRUPT  ,                  },
+    {/*3 */ idt_ent_3 , "[3] Breakpoint",                                 ACCESS_BYTE_TRAP_USER  ,                  },
+    {/*4 */ idt_ent_4 , "[4] Overflow",                                   ACCESS_BYTE_TRAP_USER  ,                  },
+    {/*5 */ idt_ent_5 , "[5] BOUND",                                      ACCESS_BYTE_TRAP_USER  ,                  },
+    {/*6 */ idt_ent_6 , "[6] Invalid Opcode (Undefined Opcode)",          ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*7 */ idt_ent_7 , "[7] Device Not Available (No Math Coprocessor)", ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*8 */ idt_ent_8 , "[8] Double Fault",                               ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*9 */ idt_ent_9 , "[9] Coprocessor Segment Overrun (reserved)",     ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*10*/ idt_ent_10, "[10] Invalid TSS",                               ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*11*/ idt_ent_11, "[11] Segment Not Present",                       ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*12*/ idt_ent_12, "[12] Stack-Segment Fault",                       ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*13*/ idt_ent_13, "[13] General Protection",                        ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*14*/ idt_ent_14, "[14] Page Fault",                                ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*15*/ NULL      , "[15] Intel reserved. Do not use",                ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*16*/ idt_ent_16, "[16] x87 FPU Floating-Point Error (Math Fault)", ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*17*/ idt_ent_17, "[17] Alignment Check",                           ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*18*/ idt_ent_18, "[18] Machine Check",                             ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*19*/ idt_ent_19, "[19] SIMD Floating-Point Exception",             ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*20*/ idt_ent_20, "[20] Virtualization Exception",                  ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*21*/ idt_ent_21, "[21] Control Protection Exception",              ACCESS_BYTE_TRAP_KERNEL,                  },
+    {/*22*/         /*  "[22] Intel reserved. Do not use",            */                                            },
+    {/*23*/         /*  "[23] Intel reserved. Do not use",            */                                            },
+    {/*24*/         /*  "[24] Intel reserved. Do not use",            */                                            },
+    {/*25*/         /*  "[25] Intel reserved. Do not use",            */                                            },
+    {/*26*/         /*  "[26] Intel reserved. Do not use",            */                                            },
+    {/*27*/         /*  "[27] Intel reserved. Do not use",            */                                            },
+    {/*28*/         /*  "[28] Intel reserved. Do not use",            */                                            },
+    {/*29*/         /*  "[29] Intel reserved. Do not use",            */                                            },
+    {/*30*/         /*  "[30] Intel reserved. Do not use",            */                                            },
+    {/*31*/         /*  "[31] Intel reserved. Do not use",            */                                            },
+};
+
 void initTss()
 {
     TssDescriptor* pTss = (TssDescriptor *) &gdtTable[SEG_TSS];
@@ -19,47 +54,13 @@ void initTss()
 void initIdt()
 {
     int i;
-    void* idtEntry[] = {
-        /*0 */ idt_ent_divide_error,
-        /*1 */ idt_ent_debug_exception,
-        /*2 */ idt_ent_nmi,
-        /*3 */ idt_ent_breakpoint,
-        /*4 */ idt_ent_overflow,
-        /*5 */ idt_ent_bound,
-        /*6 */ idt_ent_invalid_opcode,
-        /*7 */ idt_ent_device_not_available,
-        /*8 */ idt_ent_double_fault,
-        /*9 */ idt_ent_coprocessor_segment_overrun,
-        /*10*/ idt_ent_invalid_tss,
-        /*11*/ idt_ent_segment_not_present,
-        /*12*/ idt_ent_stack_segment_fault,
-        /*13*/ idt_ent_general_protection,
-        /*14*/ idt_ent_page_fault,
-        /*15*/ NULL,    /* Intel reserved. Do not use */
-        /*16*/ idt_ent_x87_fpu_floating_point_error,
-        /*17*/ idt_ent_alignment_check,
-        /*18*/ idt_ent_machine_check,
-        /*19*/ idt_ent_simd_floating_point_exception,
-        /*20*/ idt_ent_virtualization_exception,
-        /*21*/ idt_ent_control_protection_exception,
-        /*22*/ NULL,   /* 22 - 31: Intel reserved. Do not use */
-        /*23*/ NULL,   
-        /*24*/ NULL,   
-        /*25*/ NULL,   
-        /*26*/ NULL,   
-        /*27*/ NULL,   
-        /*28*/ NULL,   
-        /*29*/ NULL,   
-        /*30*/ NULL,   
-        /*31*/ NULL,   
-    };
 
     for(i = 0; i < 32; i++)
     {
-        if(NULL != idtEntry[i])
+        if(NULL != idtEntry[i].entryAddr)
         {
-            idtTable[i].gate32 = GATE_DESC_64_LOW(Selector(SEG_KERNEL_CODE), idtEntry[i], ACCESS_BYTE_TRAP, 1);
-            idtTable[i].offsetHigh = GATE_DESC_64_HIGH(idtEntry[i]);
+            idtTable[i].gate32 = GATE_DESC_64_LOW(Selector(SEG_KERNEL_CODE), idtEntry[i].entryAddr, idtEntry[i].accessByte, 1);
+            idtTable[i].offsetHigh = GATE_DESC_64_HIGH(idtEntry[i].entryAddr);
         }
         else
         {
@@ -71,45 +72,108 @@ void initIdt()
 }
 
 
+
+#define DIE()    printk("System halt!\n"); showCursor();die();
+
 void handleIdtCall(int vectorID, unsigned long errorCode, unsigned long rip)
 {
-    const char* faultName[] = {
-        "[0] Divide Error",
-        "[1] Debug Exception",
-        "[2] NMI",
-        "[3] Breakpoint",
-        "[4] Overflow",
-        "[5] BOUND",
-        "[6] Invalid Opcode (Undefined Opcode)",
-        "[7] Device Not Available (No Math Coprocessor)",
-        "[8] Double Fault",
-        "[9]  Coprocessor Segment Overrun (reserved)",
-        "[10] Invalid TSS",
-        "[11] Segment Not Present",
-        "[12] Stack-Segment Fault",
-        "[13] General Protection",
-        "[14] Page Fault",
-        "[15] Intel reserved. Do not use",
-        "[16] x87 FPU Floating-Point Error (Math Fault)",
-        "[17] Alignment Check",
-        "[18] Machine Check",
-        "[19] SIMD Floating-Point Exception",
-        "[20] Virtualization Exception",
-        "[21] Control Protection Exception",
-        "[22] Intel reserved. Do not use",
-        "[23] Intel reserved. Do not use",
-        "[24] Intel reserved. Do not use",
-        "[25] Intel reserved. Do not use",
-        "[26] Intel reserved. Do not use",
-        "[27] Intel reserved. Do not use",
-        "[28] Intel reserved. Do not use",
-        "[29] Intel reserved. Do not use",
-        "[30] Intel reserved. Do not use",
-        "[31] Intel reserved. Do not use",
-    };
+    if(vectorID <= 21)
+    {
+        color_printk(PRINT_RED, PRINT_NONE, 1, "\nA fault/trap has been triggered:\n");
+        color_printk(PRINT_YELLOW, PRINT_NONE, 1, "%s\n", idtEntry[vectorID].printInfo);
+        color_printk(PRINT_YELLOW, PRINT_NONE, 1, "At rip = %#018lx\n", rip);
+    }
 
-    color_printk(PRINT_RED, PRINT_NONE, 1, "A fault/trap was triggered!!!!\n");
-    color_printk(PRINT_RED, PRINT_NONE, 1, "%s\nerrorCode %d, rip = %#018lx\n", faultName[vectorID], errorCode, rip);
 
-    while(1);
+    switch(vectorID) {
+        case 0 ... 7:
+        case 9:
+        case 16:
+        case 18 ... 20:
+            DIE();
+            break;
+        case 8:
+        case 17:
+            /* Zero Error Code */
+            color_printk(PRINT_YELLOW, PRINT_NONE, 1, "ErrorCode = %lu\n", errorCode);
+            DIE()
+            break;
+        case 10 ... 13:
+            commonErrorCodeInfo(errorCode);
+            DIE()
+            break;
+        case 14:
+            handlePageFault(errorCode);
+            break;
+        case 21:
+            controlProtectionExceptionInfo(errorCode);
+            DIE();
+            break;
+        default:
+            color_printk(PRINT_RED, PRINT_NONE, 1, "Unknown interrupt or trap!!!!\n");
+            color_printk(PRINT_RED, PRINT_NONE, 1, "Vector ID = %d\n", vectorID);
+            DIE()
+            break;
+    }
+
+}
+
+void commonErrorCodeInfo(unsigned long errorCode)
+{
+    printk("Error code info:\n");
+
+    if(errorCode & 0x01) /* EXT */
+    {
+        printk("The exception occurred during delivery of an event external to the program, such as an interrupt or an earlier exception.\n");
+    }
+    else
+    {
+        printk("The exception occurred during delivery of a software interrupt (INT n, INT3, or INTO).\n");
+    }
+
+    if(errorCode & 0x02) /* IDT */
+    {
+        printk("The index portion of the error code refers to a gate descriptor in the IDT.\n");
+    }
+    else
+    {
+        printk("The index refers to a descriptor in the GDT or the current LDT.\n");
+        if(errorCode & 0x04) /* TI */
+        {
+            printk("The index portion of the error code refers to a segment or gate descriptor in the LDT.\n");
+        }
+        else
+        {
+            printk("The index refers to a descriptor in the current GDT.\n");
+        }
+    }
+    
+    printk("Selector index: %d\n", errorCode >> 3);
+}
+
+void handlePageFault(unsigned long errorCode)
+{
+    unsigned long cr2;
+    __asm__ __volatile__ ("movq %%cr2, %0 \n\t":"=r"(cr2)::"memory");
+
+    printk("%s\n", (errorCode & (1 <<  0)) ? "The fault was caused by a non-present page." : "The fault was caused by a page-level protection violation.");
+    printk("%s\n", (errorCode & (1 <<  1)) ? "The access causing the fault was a read." : "The access causing the fault was a write.");
+    printk("%s\n", (errorCode & (1 <<  2)) ? "A supervisor-mode access caused the fault." : "A user-mode access caused the fault.");
+    printk("%s\n", (errorCode & (1 <<  3)) ? "The fault was not caused by reserved bit violation." : "The fault was caused by a reserved bit set to 1 in some paging-structure entry.");
+    printk("%s\n", (errorCode & (1 <<  4)) ? "The fault was not caused by an instruction fetch." : "The fault was caused by an instruction fetch.");
+    printk("%s\n", (errorCode & (1 <<  5)) ? "The fault was not caused by protection keys." : "There was a protection-key violation.");
+    printk("%s\n", (errorCode & (1 <<  6)) ? "The fault was not caused by a shadow-stack access." : "The fault was caused by a shadow-stack access.");
+    printk("%s\n", (errorCode & (1 << 15)) ? "The fault is not related to SGX." : "The fault resulted from violation of SGX-specific access-control requirements.");
+
+    printk("CR2: %#018lx\n", cr2);
+
+    DIE();
+}
+
+void controlProtectionExceptionInfo(unsigned long errorCode)
+{
+    const char *CPEC[256] = {"", "NEAR-RET", "FAR-RET/IRET", "ENDBRANCH", "RSTORSSP", "SETSSBSY",};
+ 
+    printk("CPEC = %s\n", CPEC[errorCode & ((1 << 15) - 1)]);
+    if(errorCode & (1 << 15)) printk("the #CP occurred during enclave execution\n");
 }
--- kernel/trap.h
+++ kernel/trap.h
@@ -3,30 +3,39 @@
 
 extern void initTss();
 extern void initIdt();
+extern void handleIdtCall(int vectorID, unsigned long errorCode, unsigned long rip);
+extern void commonErrorCodeInfo(unsigned long errorCode);
+extern void handlePageFault(unsigned long errorCode);
+extern void controlProtectionExceptionInfo(unsigned long errorCode);
+
+typedef struct _IdtEntryInfo {
+    void* entryAddr;
+    const char* printInfo;
+    unsigned char accessByte;
+} IdtEntryInfo;
+
+extern void idt_ent_0 ();
+extern void idt_ent_1 ();
+extern void idt_ent_2 ();
+extern void idt_ent_3 ();
+extern void idt_ent_4 ();
+extern void idt_ent_5 ();
+extern void idt_ent_6 ();
+extern void idt_ent_7 ();
+extern void idt_ent_8 ();
+extern void idt_ent_9 ();
+extern void idt_ent_10();
+extern void idt_ent_11();
+extern void idt_ent_12();
+extern void idt_ent_13();
+extern void idt_ent_14();
+extern void idt_ent_16();
+extern void idt_ent_17();
+extern void idt_ent_18();
+extern void idt_ent_19();
+extern void idt_ent_20();
+extern void idt_ent_21();
 
 
-extern void idt_ent_divide_error();
-extern void idt_ent_debug_exception();
-extern void idt_ent_nmi();
-extern void idt_ent_breakpoint();
-extern void idt_ent_overflow();
-extern void idt_ent_bound();
-extern void idt_ent_invalid_opcode();
-extern void idt_ent_device_not_available();
-extern void idt_ent_double_fault();
-extern void idt_ent_coprocessor_segment_overrun();
-extern void idt_ent_invalid_tss();
-extern void idt_ent_segment_not_present();
-extern void idt_ent_stack_segment_fault();
-extern void idt_ent_general_protection();
-extern void idt_ent_page_fault();
-extern void idt_ent_x87_fpu_floating_point_error();
-extern void idt_ent_alignment_check();
-extern void idt_ent_machine_check();
-extern void idt_ent_simd_floating_point_exception();
-extern void idt_ent_virtualization_exception();
-extern void idt_ent_control_protection_exception();
-extern void ignore_int_and_halt();
-
-
+ 
 #endif
