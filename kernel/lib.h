091fbd0c7374e170b21deb3ed47f0d2c59c943e0
ValjeanTheWolfie <franciscowp@qq.com>
2020-12-26 01:45:47 +0800

    添加bool定义；die()函数

--- kernel/lib.h
+++ kernel/lib.h
@@ -5,6 +5,28 @@
 #define NULL ((void *) 0)
 #endif
 
+#ifndef _BOOL_TYPE_
+#define _BOOL_TYPE_
+typedef unsigned char bool;
+#endif
+
+#ifndef true
+#define true 1
+#endif
+
+#ifndef false
+#define false 0
+#endif
+
+
+static inline void die(void)
+{
+    while(1)
+    {
+        __asm__ __volatile__ ("hlt \n\t");
+    }
+}
+
 #define do_div(n,base) ({ \
     int __res; \
     __res = ((unsigned long) n) % (unsigned) base; \
